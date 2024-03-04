a880bd4adb04875b287014acb1f2ab43f31987ce
Paola Carr <paobloom@hotmail.com>
2024-03-04 22:04:31 +0100

    extract all info in sesctions GENERAL PROPERTY INFORMATION, and CURRENT PROPERTY APPRAISAL

--- scripts/scraper_main_beautiful.py
+++ scripts/scraper_main_beautiful.py
@@ -20,7 +20,14 @@ logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctim
 #     writer.writerow(field)
 
 # Array for column names
-header = ["Map & Parcel", "Current Owner", "Mailing Address", "Zone", "Neighborhood", "Location", "Land Area (Acres)", #"Most Recent Sale Date", "Most Recent Sale Price", "Deed Reference", "Tax District"
+header = ["Map & Parcel", "Current Owner", "Mailing Address", "Zone", "Neighborhood",
+          "Location", "Land Area (Acres)", "Most Recent Sale Date", "Most Recent Sale Price", "Deed Reference", "Tax District",
+          "Assessment Year", "Land Value", "Improvement Value", "Total Appraisal Value", 
+          "Assessment Classification", "Assessment Land", "Assessment Improvement", "Assessment Total",
+          # Improvement attributes headers:
+        #   "Building Type", "Year Built", "Square Footage", "Number of Living Units", "Building Grade", "Building Condition",
+        #   "Rooms", "Beds", "Baths", "Half Bath", "Fixtures", 
+        #   "Exterior Wall", "Frame Type", "Story Height", "Foundation Type", "Roof Cover"
              ]
 
 #Array for entries (each element in the array will be a tuple)
@@ -28,7 +35,7 @@ data = []
 
 base_url = 'https://www.padctn.org/prc/property/'
 url_suffix = '/print'
-for property_number in range(1400,1401):
+for property_number in range(1401,1402):
     url = base_url+str(property_number)+url_suffix # Build the url to take info from
     response = requests.get(url) # request website
     html = response.text # store response (html from website)
@@ -43,42 +50,30 @@ for property_number in range(1400,1401):
 
     # Check if the heading is found
     if general_info_heading:
-        # Find all <ul> elements, which contain the list elements we're interested in
-        property_info_lists = soup.find_all('ul', {'class': 'att'} ) # TODO: Delete in future if not needed (not used atm)
 
         # Retrieve the elements in sections GENERAL PROPERTY INFORMATION, and CURRENT PROPERTY APPRAISAL
         property_infos = general_info_heading.find_all_next('li', limit=20) # Remember: element w/ index 5 is an empty <li> element
-        print("property infos:")
-        print(property_infos)
 
-        index = 0
-        # Iterate through each <ul> element
-        for property_info_list in property_info_lists:
-            # Find all <li> elements within the <ul>
-            property_info_items = property_info_list.find_all('li')
+        # create row 
+        # values = []
+        # for index, item in enumerate(property_infos):
+        #     # in both cases below, split retrieved string from <li> element and only keep part after ':'
+        #     # remove unwanted spaces and periods in 'Map & Parcel' value
+        #     if index == 0:
+        #         values.append(item.text.split(':')[1].strip().replace(" ", "").replace(".", ""))
+        #     else:
+        #         # element w/ index 5 is empty so skip it
+        #         if index != 5:
+        #             values.append(item.text.split(':')[1].strip())
 
-            print()
-            print(f"Property info items {index}")
-            print(property_info_items)
-            index+=1
+        values = [item.text.split(':')[1].strip() for index, item in enumerate(property_infos) if index != 5] # does not remove unwanted spaces and periods in 'Map & Parcel' value
 
-            # Extract values from each list item
-            # values = [item.text.strip().split(':').strip() for item in property_info_items]
-            # values = [item.text.strip() for item in property_info_items]
-            # data.append(values)
+        data.append(values)
+        # print(data)
     else:
         logging.info(f"No General Info found for parcel {property_number}")
 
 
 df = pd.DataFrame(data, columns=header)
 
-df.to_csv(file_path_csv, mode='a', header=True, index=False)
-
-# # Open the CSV file and write the values to separate columns
-# with open(file_path_csv, 'a', newline='', encoding='utf-8') as csvfile:
-#     writer = csv.writer(csvfile)
-
-#     # Write the values to the CSV file
-#     writer.writerow(values)
-
-# print(f"CSV file '{file_path_csv}' created successfully.")
+df.to_csv(file_path_csv, sep=';', mode='a', header=True, index=False)
