26e17f69513c9a48dd0bddabee6220e0b4d2ed81
Paola Carr <paobloom@hotmail.com>
2024-03-11 18:45:47 +0100

    Add property number (url id) at end of row. Resume automatically where left off by reading last property number and creating new URL from there. Add log info about row number written to.

--- scripts/scraper_main.py
+++ scripts/scraper_main.py
@@ -21,7 +21,9 @@ def create_csv_file(file_path):
           "Card #",
           "Building Type", "Year Built", "Square Footage", "Number of Living Units", "Building Grade", "Building Condition",
           "Rooms", "Beds", "Baths", "Half Bath", "Fixtures", 
-          "Exterior Wall", "Frame Type", "Story Height", "Foundation Type", "Roof Cover"
+          "Exterior Wall", "Frame Type", "Story Height", "Foundation Type", "Roof Cover",
+          # Assosciated URL
+          "Property #"
              ])
 
 # Function to append a row to the CSV file
@@ -30,6 +32,26 @@ def append_row(file_path, row):
         writer = csv.writer(csv_file, delimiter=';')
         writer.writerow(row)
 
+# Function to get the next available row number
+def get_last_row_number_and_property_number(file_path):
+    with open(file_path, 'r', newline='') as csv_file:
+        reader = csv.reader(csv_file, delimiter=';')
+        rows = list(reader)
+        row_count = len(rows) # Num of written rows
+        last_scraped_property = rows[-1][-1] # last property scraped
+    return row_count, last_scraped_property  # Return the last row number written to and the property number corresponding to it
+
+
+def ensure_last_char_is_newline(file_path):
+    with open(file_path, 'ab+') as file:
+            file.seek(-1, 2)  # Move the file pointer to the end of the file
+            last_char = file.read(1)  # Read the last character
+            # Check if the last character is a newline character
+            if last_char != b'\n': # if file doesn't end in new line, append newline char to end of it
+                file.write(b'\n')
+            
+            
+
 start = time.time()
 logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     
@@ -37,9 +59,15 @@ create_csv_file(file_path_csv)
 
 base_url = 'https://www.padctn.org/prc/property/'
 url_suffix = '/print'
+    
 
-# property number: 1 to 295840
-for property_number in range(1,100):
+latest_row_number, property_num_start = get_last_row_number_and_property_number(file_path_csv) 
+property_num_start = int(property_num_start) + 1
+
+# property number: 1 to 295840 # as of 10 March 2024
+property_num_end = 295840
+
+for property_number in range(property_num_start,property_num_start+1):
     url = base_url+str(property_number)+url_suffix # Build the url to take info from
     # request website
     try:
@@ -76,9 +104,13 @@ for property_number in range(1,100):
         #         # element w/ index 5 is empty so skip it
         #         if index != 5:
         #             values.append(item.text.split(':')[1].strip())
-
+        # values = []
+        # values.append(str(property_number))
         # create row condensed in list comprehension (same code as commented out above)
         values = [item.text.split(':')[1].strip() for index, item in enumerate(property_infos) if index != 5] # does not remove unwanted spaces and periods in 'Map & Parcel' value
+        # [values.append(item.text.split(':')[1].strip()) for index, item in enumerate(property_infos) if index != 5] # does not remove unwanted spaces and periods in 'Map & Parcel' value
+
+        # print(values)
 
         # section LEGAL DESCRIPTION
         legal_description_heading = soup.find('h4', {'class': 'featurette-heading'}, string='LEGAL DESCRIPTION') # starting point
@@ -94,8 +126,10 @@ for property_number in range(1,100):
             values_card.append(str(index))
             improvement_attributes = attribute_header.find_all_next('li', limit=16)
             [values_card.append(item.text.split(':')[1].strip()) for item in improvement_attributes] #scrape the improvement attributes of the current card
+            values_card.append(property_number)
             append_row(file_path_csv, values_card)
-            logging.info(f"Parcel {property_number} successfully scraped")
+            latest_row_number+=1
+            logging.info(f"Parcel {property_number} successfully scraped to row number {latest_row_number}")
 
     else:
         logging.info(f"No General Info found for parcel {property_number}")
